b53f1da46d7724421c2243d1acf3f95846015d26
capvalen <infocat2.0@gmail.com>
2017-08-12 10:48:04 -0500

    1.2.0

--- ventas.php
+++ ventas.php
@@ -475,11 +475,7 @@ function cmbProductoListadoLlenar(){
 	}
 }
 $('#btnGuardarReporte').click(function (argument) {
-	var divToPrint=document.getElementById("printTable");
-	   newWin= window.open("");
-	      newWin.document.write(divToPrint.outerHTML);
-   newWin.print();
-   newWin.close();
+	window.print();
 });
 </script>
 
