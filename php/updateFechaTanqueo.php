09f982d3f7fbaeaaa6359a8b21bbeb2308f4dd35
Infocat <infocat2.0@gmail.com>
2018-09-27 17:24:55 -0500

    1.25 a

--- php/updateFechaTanqueo.php
+++ php/updateFechaTanqueo.php
@@ -3,7 +3,7 @@ header('Content-Type: text/html; charset=utf8');
 require("conectkarl.php");
 
 $sql= "call updateFechaTanqueo ('".$_POST['fecha']."',".$_POST['idTanq']." )";
-echo $sql;
+//echo $sql;
 if ($llamadoSQL = $conection->query($sql)) { //Ejecución mas compleja con retorno de dato de sql del procedure.
 	/* obtener el array de objetos */
 	//echo json_encode(mysqli_fetch_array($llamadoSQL, MYSQL_ASSOC)); //sólo retorna los datos.
